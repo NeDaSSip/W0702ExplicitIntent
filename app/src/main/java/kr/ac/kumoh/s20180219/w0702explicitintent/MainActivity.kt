3caed5c712b1f7d1c5b2d6f6bba2856f657a06b4
승환 김 <olleh2761@naver.com>
2022-10-19 15:17:48 +0900

    Youtube Image!

---
+++ app/src/main/java/kr/ac/kumoh/s20180219/w0702explicitintent/MainActivity.kt
@@ -0,0 +1,29 @@
+package kr.ac.kumoh.s20180219.w0702explicitintent
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kr.ac.kumoh.s20180219.w0702explicitintent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.btnExplicitIntent.setOnClickListener {
+            //Toast.makeText(this, "눌렸습니다.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, ImageActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnImplicitIntent.setOnClickListener {
+            val uri = Uri.parse("https://www.youtube.com/result?search_query" + binding.editText.text)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+    }
+}
